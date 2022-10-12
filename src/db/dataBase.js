5de02803b1d37e887b0a8729cad28d61f19f16b9
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-10-12 15:49:19 -0300

    rrota de singUp criada

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -2,8 +2,16 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
+// const connection = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: '03111999',
+  database: 'shortly_api',
 });
 
 export default connection
